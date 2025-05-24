24ad687c625b64299fc05ce79313255bbe7a5765
adam <ahelfgott@gmail.com>
2025-05-24 13:22:35 -0400

    Remove Clerk dependencies to fix build

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,48 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as auth from "../auth.js";
+import type * as campaigns from "../campaigns.js";
+import type * as dataAssets from "../dataAssets.js";
+import type * as earnings from "../earnings.js";
+import type * as marketplace from "../marketplace.js";
+import type * as recommendations from "../recommendations.js";
+import type * as simulations from "../simulations.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  campaigns: typeof campaigns;
+  dataAssets: typeof dataAssets;
+  earnings: typeof earnings;
+  marketplace: typeof marketplace;
+  recommendations: typeof recommendations;
+  simulations: typeof simulations;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
